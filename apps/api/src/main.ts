b86dc9f2cf46a73fcd15ba53c46cdc42c31e73bc
Aishwarya Mohan <aishwaryamohan310@gmail.com>
2026-05-14 13:11:37 +0200

    console to print db

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
         : ['http://localhost:3000', 'http://127.0.0.1:3000'],
     credentials: true,
   });
-
+  console.log("DATABASE_URL:", process.env.DATABASE_URL);
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
